6979bac6ddba2af0c5089cc592dc708cd9bc805b
Arth I Jani <85545030+ArthIJani@users.noreply.github.com>
2023-10-08 17:02:44 +0530

    Add files via upload

---
+++ Javascript/005_Miles_to_Kilometers.js
@@ -0,0 +1,10 @@
+// taking miles input from the user
+const miles = prompt("Enter value in miles: ")
+
+// conversion factor
+const factor = 0.621371
+
+// calculate kilometers
+const kilometers = miles / factor
+
+console.log('${miles} miles is equal to ${kilometers} kilometers.');
